b92411e04859f4e15d084594316595f878d67cc6
Afonso R. Costa Jr <arabelo@gmail.com>
2023-02-16 10:43:38 +0100

    nvim: define and use new keybindings for FTerm

--- .config/nvim/after/plugin/fterm.lua
+++ .config/nvim/after/plugin/fterm.lua
@@ -4,11 +4,17 @@ if (not status) then return end
 fterm.setup({
   border = 'double',
   dimensions  = {
-      height = 0.9,
-      width = 0.9,
+    height = 0.9,
+    width = 0.9,
   },
 })
 
+-- creating new vim commands
+vim.api.nvim_create_user_command('FTermOpen', require('FTerm').open, { bang = true })
+vim.api.nvim_create_user_command('FTermClose', require('FTerm').close, { bang = true })
+vim.api.nvim_create_user_command('FTermExit', require('FTerm').exit, { bang = true })
+vim.api.nvim_create_user_command('FTermToggle', require('FTerm').toggle, { bang = true })
+
 -- keybindings
-vim.keymap.set('n', '<C-i>', '<CMD>lua require("FTerm").toggle()<CR>')
-vim.keymap.set('t', '<C-i>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
+vim.keymap.set('n', '<leader>tt', ':FTermToggle <CR>', { noremap = true })
+vim.keymap.set('t', '<leader>tt', '<C-\\><C-n><:FTermToggle <CR>', { noremap = true })
