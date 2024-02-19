f768d49fb7de43f3ec39febc3e495ddf68ef18f1
Afonso R. Costa Jr <arabelo@gmail.com>
2024-02-19 19:02:32 +0100

    nvim: add and use the dracula theme

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add dracula
+  { "dracula/vim" },
+
+  -- Configure LazyVim to load the colorscheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "dracula",
+    },
+  },
+}
