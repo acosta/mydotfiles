a7c3a753162a42909a5bcf0fb8751ec5160586ee
Afonso R. Costa Jr <arabelo@gmail.com>
2024-02-20 16:38:45 +0100

    nvim: add keymaps to move lines using Ctrl+{j,k}

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local keymap = vim.keymap
+
+-- Moving lines using Ctrl+j (down) or Ctrl+k (up)
+-- The mappings work in normal, insert and visual modes
+keymap.set("n", "<C-j>", ":m .+1<CR>==")
+keymap.set("n", "<C-k>", ":m .-2<CR>==")
+keymap.set("i", "<C-j>", "<Esc>:m .+1<CR>==gi")
+keymap.set("i", "<C-k>", "<Esc>:m .-2<CR>==gi")
+keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv")
+keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv")
