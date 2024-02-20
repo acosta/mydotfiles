0e1b22e08323fbe770aa49c4f8632955eb6ab217
Afonso R. Costa Jr <arabelo@gmail.com>
2024-02-20 17:27:12 +0100

    nvim: trim whitespaces on file saving

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -6,23 +6,23 @@ local api = vim.api
 
 -- Setup toggle between relative and nonrelative line numbers
 local numbertogglegroup = api.nvim_create_augroup("numbertoggle", {})
-api.nvim_create_autocmd(
-  { "BufEnter", "FocusGained", "InsertLeave" },
-  {
-    pattern = '*',
-    callback = function()
-      vim.wo.relativenumber = true
-    end,
-    group = numbertogglegroup
-  }
-)
-api.nvim_create_autocmd(
-  { "BufLeave", "FocusLost", "InsertEnter" },
-  {
-    pattern = '*',
-    callback = function()
-      vim.wo.relativenumber = false
-    end,
-    group = numbertogglegroup
-  }
-)
+api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave" }, {
+  pattern = "*",
+  callback = function()
+    vim.wo.relativenumber = true
+  end,
+  group = numbertogglegroup,
+})
+api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter" }, {
+  pattern = "*",
+  callback = function()
+    vim.wo.relativenumber = false
+  end,
+  group = numbertogglegroup,
+})
+
+-- Trim whitespace onsave
+api.nvim_create_autocmd({ "BufWritePre" }, {
+  pattern = { "*" },
+  command = [[%s/\s\+$//e]],
+})
