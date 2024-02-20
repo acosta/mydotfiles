68e486b6e141f29d5a7b64adb52acd2ba4aab7d7
Afonso R. Costa Jr <arabelo@gmail.com>
2024-02-20 18:08:59 +0100

    nvim: disable the relative number toggle

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -5,21 +5,22 @@
 local api = vim.api
 
 -- Setup toggle between relative and nonrelative line numbers
-local numbertogglegroup = api.nvim_create_augroup("numbertoggle", {})
-api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave" }, {
-  pattern = "*",
-  callback = function()
-    vim.wo.relativenumber = true
-  end,
-  group = numbertogglegroup,
-})
-api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter" }, {
-  pattern = "*",
-  callback = function()
-    vim.wo.relativenumber = false
-  end,
-  group = numbertogglegroup,
-})
+-- local numbertogglegroup = api.nvim_create_augroup("numbertoggle", {})
+-- local rn_patterns = { "*.c", "*.cpp", "*.h", "*.py", "*.html", "*.ts", "*.css", "*.tsx", "*.js", "*.json", "*.lua" }
+-- api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave" }, {
+--   pattern = rn_patterns,
+--   callback = function()
+--     vim.wo.relativenumber = true
+--   end,
+--   group = numbertogglegroup,
+-- })
+-- api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter" }, {
+--   pattern = rn_patterns,
+--   callback = function()
+--     vim.wo.relativenumber = false
+--   end,
+--   group = numbertogglegroup,
+-- })
 
 -- Trim whitespace onsave
 api.nvim_create_autocmd({ "BufWritePre" }, {
