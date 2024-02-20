1382f418dfd5a62eb1be9459b92ba0c292e078e8
Afonso R. Costa Jr <arabelo@gmail.com>
2024-02-20 18:14:42 +0100

    nvim: disable backup and swap file

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local set = vim.opt
+
+set.backup = false
+set.swapfile = false
