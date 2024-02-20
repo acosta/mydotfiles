e1d347ad7b592dd4ba077416947b8b4ad83a9484
Afonso R. Costa Jr <arabelo@gmail.com>
2024-02-20 16:20:47 +0100

    nvim: toggle between relative and nonrelative line numbers

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,28 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local api = vim.api
+
+-- Setup toggle between relative and nonrelative line numbers
+local numbertogglegroup = api.nvim_create_augroup("numbertoggle", {})
+api.nvim_create_autocmd(
+  { "BufEnter", "FocusGained", "InsertLeave" },
+  {
+    pattern = '*',
+    callback = function()
+      vim.wo.relativenumber = true
+    end,
+    group = numbertogglegroup
+  }
+)
+api.nvim_create_autocmd(
+  { "BufLeave", "FocusLost", "InsertEnter" },
+  {
+    pattern = '*',
+    callback = function()
+      vim.wo.relativenumber = false
+    end,
+    group = numbertogglegroup
+  }
+)
