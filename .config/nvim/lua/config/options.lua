5e778b5f6066bef090586304bb703a3dbba374b6
Afonso R. Costa Jr <arabelo@gmail.com>
2024-02-21 10:08:17 +0100

    nvim: disable the LazyVim auto format

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Disable LazyVim auto format
+vim.g.autoformat = false
+
 local set = vim.opt
 
 set.backup = false
