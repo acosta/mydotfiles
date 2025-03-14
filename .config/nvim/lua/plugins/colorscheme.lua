c3ca02ac229e9978c9313b664e01d9c184caa0df
Afonso R. Costa Jr <arabelo@gmail.com>
2025-03-14 15:45:41 +0100

    nvim: add sonokai colorscheme and configure everforest
    Make everforest use the light background.

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -3,7 +3,18 @@ return {
   { "dracula/vim" },
 
   -- add everforest
-  { "sainnhe/everforest" },
+  { "sainnhe/everforest",
+    config = function ()
+      vim.go.background = "light"
+    end
+  },
+
+  -- add sonokai
+  { "sainnhe/sonokai",
+    config = function ()
+      vim.go.sonokai_style = "andromeda"
+    end,
+  },
 
   -- Configure LazyVim to load the colorscheme
   {
